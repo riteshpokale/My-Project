55ec147f7826eed040cc2c0f909f61877c795ae3
accawsdevops <acc.aws.devops@gmail.com>
2025-03-15 09:36:45 +0530

    t

--- script/install.sh
+++ script/install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 sudo systemctl stop nginx
-echo "Moving build files to Nginx directory..."
-sudo mv /home/ec2-user/nodejsbuild/build/* /usr/share/nginx/html/
+echo "Clearing old build files..."
+sudo rm -rf /usr/share/nginx/html/*
+
+echo "Copying new build files to Nginx directory..."
+sudo cp -r /home/ec2-user/nodejsbuild/build/* /usr/share/nginx/html/
 
 echo "Restarting Nginx..."
 sudo systemctl start nginx
