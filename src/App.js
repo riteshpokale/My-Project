b6014f0dd752388e5253f97c83a2337f539cf219
Ritesh Pokale <riteshspoklae27@gmail.com>
2025-05-16 23:07:07 +0530

    updating some changes in buildspec.yml and app.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css'; // Importing the CSS file for styles
 const App = () => {
   return (
     <div className="container">
-      <h1 className="text">Welcome to the Accleration Training Center...pune </h1>
+      <h1 className="text">Welcome to the Accleration Training Center  </h1>
     </div>
   );
 };
